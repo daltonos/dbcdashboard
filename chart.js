506777c8974e45a662ba95f42240dc3cc6c268be
David <33631446+David-RHP@users.noreply.github.com>
2018-06-27 16:37:21 +0200

    changed unit label and data labels in Chart.js

--- chart.js
+++ chart.js
@@ -2,9 +2,9 @@
 var chart = c3.generate({
     data: {
         columns: [
-            ['In', 70],
+            ['in', 70],
             ['base', 120],
-            ['Out', 100],
+            ['out', 100],
             ['max', 80],
             ['min', 20]
         ],
@@ -32,7 +32,7 @@ var chart = c3.generate({
         },
     min: 0, // 0 is default, //can handle negative min e.g. vacuum / voltage / current flow / rate of change
     max: 120, // 100 is default
-    units: 'K-Euros',
+    units: 'in Mio €',
  // for adjusting arc thickness
     },// the three color levels for the percentage values.
     size: {
